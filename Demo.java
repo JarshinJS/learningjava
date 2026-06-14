590214b8592bed2d1822dba2c6d16f8f89b9c15c
JarshinJS <jarshinjohnson@gmail.com>
2026-06-14 17:29:22 +0530

    updated

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo{
     public static void main(String[] args){
-        System.out.print("I like coffee!!");
+        System.out.print("I like coffee!!\n");
+        System.out.println("It's very good!!");
+
     }
 }
