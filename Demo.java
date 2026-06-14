6d193e76548527217bc2c4cda171475e79f1ed22
JarshinJS <jarshinjohnson@gmail.com>
2026-06-14 17:31:44 +0530

    comment line added

--- Demo.java
+++ Demo.java
@@ -1,4 +1,11 @@
 class Demo{
+
+    // this is my first java program
+    /* i 
+    have 
+    learned
+    some thing about 
+    java */
     public static void main(String[] args){
         System.out.print("I like coffee!!\n");
         System.out.println("It's very good!!");
